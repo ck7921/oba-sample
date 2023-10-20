77af968a1f428208dfb230d8c9ede26df797f60f
Chris <ckantner@gmail.com>
2023-10-20 11:33:19 +0200

    OBA: ui look re-worked

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import {NgModule} from "@angular/core";
 import {AuthenticationComponent} from "./authentication/authentication.component";
 import {ConsentComponent} from "./consent/consent.component";
 import {TransactionsComponent} from "./transactions/transactions.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
 const routes: Routes = [
+  { path: '', component: WelcomeComponent },
   { path: 'balances', component: BalancesComponent },
   { path: 'authentication/:action', component: AuthenticationComponent },
   { path: 'consent/:consentId', component: ConsentComponent },
